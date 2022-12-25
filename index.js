d10339bca228a25367a35b8bfbd8ba4ea0652914
shreeraj-shinde <shreerajshinde86@gmail.com>
2022-12-25 11:36:51 +0530

    user route created

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require("express");
 
+const { users } = require("./data/users.json");
+
+const { books } = require("./data/books.json");
+
 const app = express();
 
 const port = 8081;
@@ -9,6 +13,26 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.status(200).send({
     message: "Server is Started",
+  });
+});
+
+/**
+ * Route : /users
+ * Method : GET
+ * Description : Get all users
+ * Access : Public
+ * Parameters : None
+ */
+app.get("/users", (req, res) => {
+  res.status(200).send({
+    Sucess: True,
+    data: users,
+  });
+});
+
+app.all("/", (req, res) => {
+  res.status(404).send({
+    "": "Not Found",
   });
 });
 
