08d4ce903c9587c91bd2290f5fa2fc7cf3202712
shreeraj-shinde <shreerajshinde86@gmail.com>
2022-12-28 14:09:44 +0530

    completed all routes

--- routes/users.js
+++ routes/users.js
@@ -117,4 +117,68 @@ router.delete("/:id", (req, res) => {
   });
 });
 
+/**
+ * Route : /users/subscription-details/:id
+ * Method : GET
+ * Description : Getting User Subscription Details
+ * Access : Public
+ * Parameters : id
+ */
+router.get("/subscription-details/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users.find((each) => each.id === id);
+  if (!user) {
+    return res.status(404).send({
+      success: "False",
+      message: "USER NOT FOUND",
+    });
+  }
+  const getDateInDays = (date = "") => {
+    if (date === "") {
+      //Current Date
+      date = new Date();
+    } else {
+      //Date on the basis of data variable
+      date = new Date(date);
+    }
+    let Days = Math.floor(date / (1000 * 60 * 60 * 24));
+    return Days;
+  };
+  const getSubscriptionType = (Days) => {
+    if (user.subscriptionType === "Basic") {
+      Days = Days + 90;
+    } else if (user.subscriptionType === "Standard") {
+      Days = Days + 180;
+    } else if (user.subscriptionType === "Premium") {
+      Days = Days + 365;
+    }
+    return Days;
+  };
+
+  // Subscription expiration calculation
+  // january 1,1970,UTC. // milliseconds
+  let returnDate = getDateInDays(user.returnDate);
+  let currentDate = getDateInDays();
+  let subscriptionDate = getDateInDays(user.subscriptionDate);
+  let subscriptionExpiration = getSubscriptionType(subscriptionDate);
+
+  const Data = {
+    ...user,
+    subscriptionExpired: subscriptionExpiration < currentDate,
+    daysLeftforExpiration:
+      subscriptionExpiration <= currentDate
+        ? 0
+        : subscriptionDate - currentDate,
+    fine:
+      returnDate < currentDate
+        ? subscriptionExpiration <= currentDate
+          ? 200
+          : 100
+        : 0,
+  };
+  res.status(200).send({
+    success: "True",
+    data: Data,
+  });
+});
 module.exports = router;
